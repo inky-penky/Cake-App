1865802e17dc69c64ec6fd15514b867123fc15f6
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2022-10-05 21:50:31 +0100

    feat: reducer setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './components/redux/cake/Store';
 import './App.css';
 import CakeContainer from './components/CakeContainer';
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
+      <div className="App">
       <CakeContainer/>
     </div>
+    </Provider>
   );
 }
 
